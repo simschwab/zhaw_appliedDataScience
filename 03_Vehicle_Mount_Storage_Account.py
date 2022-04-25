8d6f81ec38ea3f0aca620014524d18f10bc6ab16
Mario Locher <lochemar@students.zhaw.ch>
2022-04-25 08:08:24 +0200

    Merge branch 'main' of https://github.com/simschwab/zhaw_applieddatascience

---
+++ 03_Vehicle_Mount_Storage_Account.py
@@ -0,0 +1,91 @@
+# Databricks notebook source
+# MAGIC %md <h1>Mount Storage Account to access vehicle images</h1>
+
+# COMMAND ----------
+
+# MAGIC %md <h5>Set parameters</h5>
+
+# COMMAND ----------
+
+storageaccount="zhawadsstorage"
+
+account_key="BEPIWVQrb1Iw4rwPDe9oyzuwwF4MtTFL/uvmrMqPPD7T7jgi5s0dr8W5axdT3LlP4mCFwLOBLKo3+AStKTvmSw=="
+
+containername="vehicles100"
+#mountname=containername
+mountname = "100vehiclessmall"
+
+# COMMAND ----------
+
+# MAGIC %md <h5>1. Mount blob storage</h5>
+
+# COMMAND ----------
+
+if any(mount.mountPoint == "/mnt/" + mountname for mount in dbutils.fs.mounts()):
+  print ("directory " + "/mnt/" + mountname + " is already mounted")
+else:
+  print ("In case you have a cluster with 0 workers, you need to cancell statement manually after 30 seconds. This is because a spark job is started, which cannot be executed since there are 0 workers. However, the storage is mounted, which can be verified by rerunning cell")
+  dbutils.fs.mount(
+  source = "wasbs://" + containername + "@" + storageaccount + ".blob.core.windows.net",
+  mount_point = "/mnt/" + mountname,
+  extra_configs = {"fs.azure.account.key." + storageaccount +".blob.core.windows.net":account_key})
+
+# COMMAND ----------
+
+# MAGIC %md <h5>2. Unzip pictures in storage account</h5>
+
+# COMMAND ----------
+
+#2. unzip data
+
+import zipfile
+import os
+
+#datafile = "vehicledata.zip"
+#with 100 pictures per categorie
+datafile = "trainingdata100.zip"
+
+datafile_dbfs = os.path.join("/dbfs/mnt/" + mountname, datafile)
+
+zip_ref = zipfile.ZipFile(datafile_dbfs, 'r')
+zip_ref.extractall("/dbfs/mnt/" + mountname)
+zip_ref.close()
+
+
+# COMMAND ----------
+
+# MAGIC %md  <h5>3. List and show pictures</h5>
+
+# COMMAND ----------
+
+#3. Show pictures
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+from PIL import Image
+import os
+
+categoriesList=['ambulance', 'bicycle', 'bus', 'car', 'limousine', 'motorcycle', 'tank', 'taxi', 'truck', 'van']
+
+def plotImagesMount(n_images=8):
+    fig, axes = plt.subplots(n_images, n_images, figsize=(9,9))
+    axes = axes.flatten()
+    
+    for i in range(n_images * n_images):
+        rand1 = random.randint(0, 6)
+        rand2 = random.randint(1020, 1030)
+        filename=str(categoriesList[rand1]) + "_" + str(rand2)
+        filenamejpg=filename + ".jpg"
+        img = Image.open(os.path.join("/dbfs/mnt/" + mountname + "/", filenamejpg))
+        ax = axes[i]
+    
+        ax.imshow( img, cmap="Greys_r")
+        ax.set_xticks(())
+        ax.set_yticks(())
+        
+        ax.set_title(filename, fontsize=18 - n_images)
+        
+    plot = plt.tight_layout()
+    return plot
+  
+display(plotImagesMount(n_images=5))
--- TODO.md
+++ TODO.md
@@ -2,17 +2,18 @@
   
 ## Basic Requirements:
 (1) Datenerhebung mittels Web Scraping oder API
-- [ ] API
-- [ ] web scraping
+- [x] API
+  - [ ] getr kaggle vehicle dataset
+- [x] web scraping
 
 (2) Datenaufbereitung (dazu können Methoden aus Data Science Fundamantals verwendet werden)
-- [ ] xxx
+- [x] xxx
 
 (3) Verwendung eines ML Frameworks/Library (tensorflow/keras, sklearn oder im Kurs nicht betrachtete wie pytorch)
 - [ ]
 
 (4) Versionierung und Verfügbarmachen der Daten und des Modells über GitHub
-- [ ] Github & Azure
+- [x] Github & Azure
 
 
 ## Zusatzpunkte für Projektarbeit, falls folgendes erfüllt (max. 5 Punkte):
@@ -21,13 +22,30 @@ Kreativität der Umsetzung (kreativ ist alles, was in den Lektionen und Übungen
 - [ ] xxx
 
 Verwendung von sowohl Web Scraping als auch API's für den Datenbezug
-- [ ] siehe oben
+- [x] siehe oben
 
 Verwendung von Docker oder eines Cloud-Services (z.B. EC2-Instance, Jupyter-Notebook via Sagemaker)
-- [ ] Azure
+- [x] Azure
 
 Verwendung einer fortgeschrittenen Deep Learning Struktur/Technik (z.B.CNN, RNN, Transfer Learning, Transformers…)
-- [ ] xxx
+- [x] xxx
 
 Forschungsbasierte Analyse der ethischen Fragestellung und potenzielle Risiken/Bias des entwickelten Produkts
 - [ ] xxx
+
+## Abgabe
+Projektvortrag in Form einer Videoaufzeichnung (max. 3 Punkte)
+Bewertet werden:
+- Qualität der Präsentation
+- Struktur der Präsentation
+- Zeiteinhaltung (Länge der Präsentation (min.) = Anzahl Studierende pro Gruppe x 5 Min.)
+
+Abzugeben sind als .zip-Datei mit der Gruppennummer, z.B. Projektarbeit_ADS_FS2022_Gruppe_02.zip
+1.) Videoaufzeichnung Projektvortrag als mp4-Datei, z.B. Videoaufzeichnung_Gruppe_02.mp4
+2.) Präsentation als PDF-File (als z.B. Praesentation_Gruppe_02.pdf) mit folgender Minimalstruktur:
+     - Titel und voller Name der Studierenden
+     - Einleitung (Hintergrund, Problemstellung, Zielstellung, Forschungsfrage)
+     - Methode & Vorgehensweise
+     - Diskussion & Ergebnisse
+     - Schlussfolgerungen
+     - Link zu Github repository mit allen Materialien (Daten, Jupyter Notebooks, etc.)
